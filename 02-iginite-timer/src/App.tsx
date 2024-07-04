dc012a55d1a8b07aa7277ffda15aad0955e849ed
dcm <davimarque2000@gmail.com>
2024-07-03 21:32:40 -0300

    set theme

--- 02-iginite-timer/src/App.tsx
+++ 02-iginite-timer/src/App.tsx
@@ -1,12 +1,14 @@
 import { Button } from "./components/Button/Button";
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <Button variant="danger" />
       <Button variant="primary" />
       <Button variant="secondary" />
       <Button />
-    </>
+    </ThemeProvider>
   );
 }
